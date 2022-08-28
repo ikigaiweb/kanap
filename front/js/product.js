50e46395a46b372269544fb64191f704a78b35e9
stephane Gamot <lavoiedubienetre@outlook.com>
2022-08-28 20:25:58 +0200

    28 08 22
    mise en place de la page index.js  et debut de la product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,2 +1,57 @@
-fetch('http://localhost:3000/api/products')
-.then(res => console.log(res))
+
+const searchParams = new URLSearchParams(window.location.search);   // ***Recuperation de L'ID de l'URL * peut etre rajouter windows 
+const productID = searchParams.get('id');
+
+const infoProductApi = async function () {       // async fait qu'une fonction renvoie une promesse * await fait qu'une fonction attend une Promise
+    await fetch(`http://localhost:3000/api/products/${productID}`)  // Methode Fetch qui me permet de recuperer les données du serveur
+    .then(reponse => reponse.json())               // Promesse d'une reponse en Json  forme flechée
+    .then(function(data){                          // ***une promesse de pouvoir "loguer" les datas (mes données)
+        return (produits = data);                  // demande d'un retour de reponse (les datas) que je "nomme" produits, products etant deja sur une autre page possibilité de confusion
+        })
+        .catch (function (_Error){                               // Si ma promesse initiale est rejetée 'catch'
+            console.log(`J'en connais Un qui s'est planté`);    // s'il apparait dans la console, il y a un problème
+        });
+};
+
+async function miseEnPlaceDuProduit() {               // Creation d'une fonction qui permet l'affichage dans la page
+    await infoProductApi();                           // pour pouvoir mettre en place mon produit je dois d'abord appeler mes info provenant de l'API
+    let SVP_choisissezUneCouleur = document.querySelector("#colors");   // ***pour     j'ai du creeer une variable *** pourquoi un #           
+    produits.colors.forEach((color) => {              //   
+        SVP_choisissezUneCouleur.innerHTML +=  `<option value="${color}">${color}</option>`;
+    })
+    document.getElementsByClassName("item__img").innerHTML = `<img src="${produits.imageUrl}" alt="${produits.altTxt}">`;  // ***verifier entre produits et products entre les 2 pages le meme ou pas ? 
+    document.getElementById("title").textContent = produits.name;  // liaison entre #title et le nom du produit qui est sous le nom name dans le Json
+    document.getElementById("price").textContent = produits.price; // liaison entre #price (html) et le nom du produit qui est dans le Json
+    document.getElementById("description").textContent = produits.description; // liaison entre #price (html) et le nom du produit qui est dans le Json
+              
+     
+}; 
+
+
+
+
+
+
+miseEnPlaceDuProduit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Variable making
+
+const addToCartBtn = document.getElementById("addToCart");
+const colorBtn = document.getElementById("colors");
+const quantity = document.getElementById("quantity");
+let colorAttr, qtyAttr, actualItemIndex;
+*/
--- front/js/script.js
+++ front/js/script.js
@@ -1,2 +1,54 @@
-fetch('http://localhost:3000/api/products')
-.then(res => console.log(res))
+const items = document.getElementById(`items`);        // ***informer où ce sera afficher en l'occurence là où #items sera
+
+const infoKanapApi = async function () {       // async fait qu'une fonction renvoie une promesse * await fait qu'une fonction attend une Promise
+    await fetch("http://localhost:3000/api/products")  // Methode Fetch qui me permet de recuperer les données du serveur
+        .then(function(response){                      // Promesse de recevoir une réponse de l'api 
+            console.log(response);                     // verification que je recois bien la réponse
+            return response.json();                    // Demande de retour de la réponse .. en format Json 
+        })                                             // En + simple .then(reponse => reponse.json())
+        .then(function(data){                          // une promesse de pouvoir "loguer" les datas (mes données)
+            return (products = data);                  // demande d'un retour de reponse (les datas) que je "nomme" products
+        })
+        .catch (function (Error){                               // Si ma promesse initiale est rejetée 'catch'
+            console.log(`J'en connais Un qui s'est planté`);    // s'il apparait dans la console, il y a un problème
+        });
+};
+
+async function miseEnPlaceProduitSurLaPage() {       // Creation d'une fonction qui permet l'affichage dans la page
+    await infoKanapApi();                            // pour pouvoir mettre en place mes kanaps je dois d'abord appeler mes info provenant de l'API
+    products.forEach((product) => {                  // Me permet d'exécuter la fonction sur chaque élément product du tableau products.
+        items.innerHTML +=                           // Me permet d'inserer mon HTML dans la variable ..... #items
+          ` <a href="./product.html?id=${product._id}">  
+            <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}" />
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>  
+            </article>
+            </a>`;
+    });  // litteraux de gabarit ${} il va amener à "/product.name" du Json de chaque élément du tableau (voir le forEach)
+}
+miseEnPlaceProduitSurLaPage();                     //appel de la fonction qui permetra l'affichage
+
+
+// Une fonction asynchrone est une fonction qui s'exécute de façon asynchrone grâce à la boucle d'évènement en utilisant une promesse (Promise) comme valeur de retour.
+
+// Une promesse représente une valeur qui peut être disponible maintenant, dans le futur voire jamais.
+// La méthode then() renvoie un objet Promise. Elle peut prendre jusqu'à deux arguments qui sont deux fonctions callback à utiliser en cas de complétion ou d'échec de la Promise.
+      /* const promise1 = new Promise((resolve, reject) => {
+      resolve('Success!');
+       });
+
+      promise1.then((value) => {
+      console.log(value);
+      // expected output: "Success!"
+      });
+      */
+
+// La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.  
+// forEach exécute la fonction callback une fois pour chaque élément
+// Ces espaces sont indiqués par le signe dollar ($) et des accolades (${expression}). Les expressions dans les espaces réservés et le texte compris dans ces espaces sont passés à une fonction.
+// Les littéraux de gabarits sont des littéraux de chaînes de caractères permettant d'intégrer des expressions.
+   //  Les gabarits sont délimités par des caractères accent grave.
+   //  Ces espaces sont indiqués par le signe dollar ($) et des accolades (${expression})
+
+
