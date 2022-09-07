f85ae9261fd0b17e0bc5946096718661fc1670b3
stephane Gamot <lavoiedubienetre@outlook.com>
2022-09-07 22:23:11 +0200

    cart 50%

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,40 +1,171 @@
+let basket = JSON.parse(localStorage.getItem("localBasket"));    
+      console.log(basket);
 
-async function infoProductApi () {
-    const response = await fetch(`http://localhost:3000/api/products/` + productID)
- if(response.ok) {
-    return response.json.son();
-} 
+      async function infoProductApis(id) {
+        let response = await fetch(`http://localhost:3000/api/products/${id}` )
+       if (response.ok) {                                                              
+          return response.json();                                                      
+        } else {
+            console.log(`J'en connais Un qui s'est planté`);                           
+        }
+      }
+      
+
+ async function displayCart() {
+   //await infoProductApis(basket[i].id);   
+    console.log("3");
+    
+    if (basket == null ) {
+      document.querySelector("h1").textContent = "Votre panier est vide";
+    }
 else {
-    console.log(response.error);
+    for (let i = 0; i < basket.length; i++) {
+        let item = basket[i];
+        let info = await infoProductApis(item.id);
+        console.log(info);
+
+
+const cartItems = document.getElementById("cart__items");
+
+const article = document.createElement("article");   
+article.classList = "cart__item";
+article.setAttribute("data-id", `${item.id}`);
+article.setAttribute("data-color",`${item.color}`);
+console.log("5");
+cartItems.appendChild(article);
+
+
+const cartItemImg = document.createElement("div");
+cartItemImg.classList = "cart__item__img";
+article.appendChild(cartItemImg);
+
+console.log("6");
+
+const img = document.createElement("img"); 
+cartItemImg.appendChild(img);
+img.src = info.imageUrl;
+img.alt = info.altTxt;
+
+console.log("7");
+
+const cartItemContent = document.createElement("div");
+cartItemContent.classList = "cart__item__content";
+article.appendChild(cartItemContent);
+console.log("8");
+
+const cartItemContentDescription = document.createElement("div");
+cartItemContentDescription.classList = "cart__item__content__description";
+cartItemContent.appendChild(cartItemContentDescription);
+console.log("9");
+
+const title = document.createElement("h2");
+cartItemContentDescription.appendChild(title);
+title.innerHTML = info.name;
+console.log("10");
+
+const couleur = document.createElement("p");
+cartItemContentDescription.appendChild(couleur);
+document.querySelector("p").innerHTML = item.color;
+console.log("11");
+
+const prix = document.createElement("p");
+cartItemContentDescription.appendChild(prix);
+prix.innerHTML = info.price + " €";
+console.log("12");
+
+const cartItemContentSettings = document.createElement("div");
+cartItemContentSettings.classList = "cart__item__content__settings";
+cartItemContent.appendChild(cartItemContentSettings);
+
+const cartItemContentSettingsQuantity = document.createElement("div");
+cartItemContentSettingsQuantity.classList = "cart__item__content__settings__quantity";
+cartItemContentSettings.appendChild(cartItemContentSettingsQuantity);
+
+const quantite = document.createElement("p");
+cartItemContentSettingsQuantity.appendChild(quantite);
+quantite.innerHTML = "Qté : ";
+
+const itemQuantity = document.createElement("input");
+itemQuantity.type = "number";
+itemQuantity.classList = "itemQuantity";
+itemQuantity.name = "itemQuantity";
+itemQuantity.min = "1";
+itemQuantity.max = "100";
+itemQuantity.value = item.quantity;
+cartItemContentSettingsQuantity.appendChild(itemQuantity);
+//itemQuantity.innerHTML = info.quantity;
+
+const cartItemContentSettingsDelete = document.createElement("div");
+cartItemContentSettingsDelete.classList.add("cart__item__content__settings__delete");
+cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
+
+const deleteItem = document.createElement("p");
+deleteItem.classList.add("deleteItem");
+cartItemContentSettingsDelete.appendChild(deleteItem);
+deleteItem.innerHTML = "annuler votre commande";
+
+
+    }}    }
+   displayCart();
+/*
+
+
+
+
+
+
+img.src = 
+
+
+    }
+
+
+
+const newH2 = document.createElement("h2"); 
+article.appendChild(newH2);
+newH2.textContent = "sdrger";
+
+
+
+//const productID = document.createElement("").cartItems;     
+//productID.textContent = "ha ha ha ah a";
 }
 
-}
+*/
 
+/*
+        const a = document.creatELEMENT("a");
+        a.href = ""
+        a.innerHTML =
+        */  /* 
+        <section id="cart__items">
+        <article
+
+
+/*
+
+let urlparam = new URLSearchParams(window.location.search);    // ***Recuperation de L'ID de l'URL
+let productID = urlparam.get("id");                            // Cette technique me permet de localiser l'ID et donc dans la foction suivante n'appeler que lui !
+
+    async function infoProductApi() {
+        let response = await fetch("http://localhost:3000/api/products/" + productID)   // Methode Fetch qui me permet de recuperer les données du serveur * Et là l'ID demandé plus haut 
+       if (response.ok) {  
+          return response.json();                                                       // Alors il renvoie une reponse sous format JSON
+        } else {
+            console.log(`J'en connais Un qui s'est planté`);                            // Si Erreur alors message dans le console.log
+        }
+      }
+      function getBasket (basket) {
+      let basket = JSON.parse(localStorage.getItem("localBasket"));    
+     console.log(basket);
+      }
 
 function saveBasket (basket) {                                // la fonction a pour nom "saveBasket" et parametre (basket) ce qui signifie qu'elle ne s'apliquera que à Basket 
-    localStorage.setItem("basket", JSON.stringify(basket));   // JSON.stringify va permetre de transformé un tableau ou objet complexe en chaine de charactere. *** le "basket" est le mot clé
-}                                                             // dans ce cas transformer en "string" (mots)  BASKET 
- function getBasket () {
-    let basket = localStorage.getItem("basket");
-    if (basket ==null) {                 // si mon panier est vide alors il retournera un tableau vide
-        return [];
-    } else {
-        return JSON.parse(basket);       // Si il n'est pas vide JSON.parse va lui à l'inverse remmetre le produit en tableau pu autre depuis 
-    }
-  
- }
- function addBasket (product) {                // on va creer une fonction pour ajouter 
-    let basket = getBasket ();
-    let foundProduct = basket.find(p => p.id == productID.id);     //.find est une fonction qui travaille sur les tableaux, qui permet de chercher un element sur un tableau par rapport a une condition (je cherche dans mon panier si il y a un produit dont l'ID est = au produit que je veux ajouter)  )
-    if(foundProduct != undefined) {             // != signifie different *** si il n'en n 'a pas trouve il retournera "undefined", si il y en a un alors il aura retourné l'element demandé
-        foundProduct.quantity++;                // et donc j'ajoute + 1 à la quantité
-    } else {
-        product.quantity = 1;                  // je defini par defaut mon produit à 1
-        basket.push(product);                  // et ensuite je peux pousser mon produit 
-    }
-    saveBasket(basket);                 // apres l'ajout on enregistre le nouveau panier
- }
- function removeFromBasket (product) {               // pour retirer un produit du panier pas juste 1 mais tout un produit
+    localStorage.setItem("localBasket", JSON.stringify(basket));   // JSON.stringify va permetre de transformé un tableau ou objet complexe en chaine de charactere. *** le "basket" est le mot clé
+}      
+
+
+function removeFromBasket (basket) {               // pour retirer un produit du panier pas juste 1 mais tout un produit
     let basket = getBasket ();                        // on rapelle le variable (il etait dans une fonction)
     basket = basket.filter(p => p.id != productK.id); // fonction .filter va permetre de filtrer un tableau en fonction d'une condition *** on peut le faire avce == ou != 
     saveBasket(basket);                               // On resauve ce nouveau resultat
@@ -53,6 +184,33 @@ function saveBasket (basket) {                                // la fonction a p
 
     }    
  }
+
+
+
+/*
+// dans ce cas transformer en "string" (mots)  BASKET 
+ function getBasket () {
+    let basket = localStorage.getItem("basket");
+    if (basket ==null) {                 // si mon panier est vide alors il retournera un tableau vide
+        return [];
+    } else {
+        return JSON.parse(basket);       // Si il n'est pas vide JSON.parse va lui à l'inverse remmetre le produit en tableau pu autre depuis 
+    }
+ }*/
+/*
+ function addBasket (product) {                // on va creer une fonction pour ajouter 
+    let basket = getBasket ();
+    let foundProduct = basket.find(p => p.id == productID.id);     //.find est une fonction qui travaille sur les tableaux, qui permet de chercher un element sur un tableau par rapport a une condition (je cherche dans mon panier si il y a un produit dont l'ID est = au produit que je veux ajouter)  )
+    if(foundProduct != undefined) {             // != signifie different *** si il n'en n 'a pas trouve il retournera "undefined", si il y en a un alors il aura retourné l'element demandé
+        foundProduct.quantity++;                // et donc j'ajoute + 1 à la quantité
+    } else {
+        product.quantity = 1;                  // je defini par defaut mon produit à 1
+        basket.push(product);                  // et ensuite je peux pousser mon produit 
+    }
+    saveBasket(basket);                 // apres l'ajout on enregistre le nouveau panier
+ }
+*//*
+
  function getNumberProduct (){                                      // cela nous donnera la quantité de produit qui se trouve dans le panier
     let basket = getBasket ();                                      // on recupere le panier
     let number = 0;                                                 // on commence à 0 
@@ -72,7 +230,7 @@ function saveBasket (basket) {                                // la fonction a p
 
 
 
-
+*/
 
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,128 +1,67 @@
+let urlparam = new URLSearchParams(window.location.search);    // ***Recuperation de L'ID de l'URL
+let productID = urlparam.get("id");                            // Cette technique me permet de localiser l'ID et donc dans la foction suivante n'appeler que lui !
 
-const searchParams = new URLSearchParams(location.search);   // ***Recuperation de L'ID de l'URL * peut etre rajouter windows 
-const productID = searchParams.get('id');                    // Cette technique me permet de localiser l'ID et donc dans la foction suivante n'appeler que lui !
+    async function infoProductApi() {
+        let response = await fetch("http://localhost:3000/api/products/" + productID)   // Methode Fetch qui me permet de recuperer les données du serveur * Et là l'ID demandé plus haut 
+       if (response.ok) {                                                               // Si reponse (nom du variable est ok.... 
+          return response.json();                                                       // Alors il renvoie une reponse sous format JSON
+        } else {
+            console.log(`J'en connais Un qui s'est planté`);                            // Si Erreur alors message dans le console.log
+        }
+      }
 
-async function infoProductApi (id) {
-    const response = await fetch(`http://localhost:3000/api/products/${productID}`)
-    .then (response => response.json())                                         // Promesse d'une reponse en Json  forme flechée
-    .then (data => (product = data))                                           // avant  .then(function(data){ return (produits = data); })  * le ZZ c 'est juste parce que vs code n'aime pas juste "product"
-    .catch (error => (error = console.log(`J'en connais Un qui s'est planté`)))  // pas besoin de redeclarer l'error mais bon pour l'entrainement des fonctions flechées     
-    /*
- if(response.ok) {
-    return response.json.son();
-} 
-else {
-    console.log(response.error);
-}
-*/ 
-}
+async function displayInfo() {
+  const kanapInfo = await infoProductApi();                                     // Creation variable = l'appel de la fonction d'avant / le nom du variable sera utilise juste apres 
+    document.getElementById("title").textContent = kanapInfo.name;              // liaison entre #title et le nom du produit qui est sous le nom name dans le Json                    
+    document.getElementsByClassName("item__img")[0].innerHTML = `<img src="${kanapInfo.imageUrl}" alt="${kanapInfo.altTxt}">`;  // [0] me permet d'aller chercher le 1er element de la .class item__img...         //   variable connécté a lemplacement de l'image
+    document.getElementById("price").textContent = kanapInfo.price;             // liaison entre #price (html) et le nom du produit qui est dans le Json                      //   variable connécté a lemplacement du prix
+    document.getElementById("description").textContent = kanapInfo.description; // liaison entre #description (html) et le nom du produit qui est dans le Json           //   variable connécté a lemplacement de la description
 
+    kanapInfo.colors.forEach((color) => {                                       //   La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
+    document.querySelector("#colors").innerHTML +=  `<option value="${color}">${color}</option>`;  // Les gabarits sont délimités par des caractères accent grave (` `) TOUT CE QUI RENVOIE UNE VALEUR VA ETRE REMPLACE PAR SA VALEUR
+}) 
+    const itemQuantity = document.getElementById("quantity");    // Creation du variable en liaison à la page Html #quantity
+    const ColorSet = document.getElementById("colors");          // Creation du variable en liaison à la page Html #colors
+    const  addToTheCart = document.getElementById("addToCart");  // Creation du variable en liaison à la page Html #addToCart
+  addToTheCart.addEventListener('click', (event) => {            // Création d'une ecoute evenement (le click du bouton #addToCart)
+ event.preventDefault();                                         // Je l'empeche momentanement de faire son travail  
 
-async function productData() {
-    const production = await infoProductApi ();                         // pour pouvoir mettre en place mon produit je dois d'abord appeler mes info provenant de l'API
-    let kanapTitle = document.getElementById("title");                      //   Je les ai sorti de la fonction pour pouvoir m'en reservir + tard dans le "addtothecart"
-    let kanapImg = document.getElementsByClassName("item__img")[0];         //   variable connécté a lemplacement de l'image
-    let kanapPrice = document.getElementById("price");                      //   variable connécté a lemplacement du prix
-    let kanapDescription = document.getElementById("description");          //   variable connécté a lemplacement de la description
+    if (itemQuantity.value > 0 && itemQuantity.value < 101 && ColorSet.value != "") {  // Si le chiffre indique par le client est superieur ou egal à 0 et inferieur à 100 et qu'il a une couleur choisie 
+      let kanapItem = {                                                                  // Creation d'un objet qui liste les produits demandé
+        id: productID,
+        color: ColorSet.value,
+        quantity: itemQuantity.value,
+      };
 
-    kanapImg.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;  // [0] me permet d'aller chercher le 1er element de la .class item__img... 
-    kanapTitle.textContent = product.name;              // liaison entre #title et le nom du produit qui est sous le nom name dans le Json
-    kanapPrice.textContent = product.price;             // liaison entre #price (html) et le nom du produit qui est dans le Json
-    kanapDescription.textContent = product.description; // liaison entre #description (html) et le nom du produit qui est dans le Json 
-    product.colors.forEach((color) => {                               //   La méthode forEach() permet d'exécuter une fonction donnée sur chaque élément du tableau.
-        kanapColor.innerHTML +=  `<option value="${color}">${color}</option>`;  // Les gabarits sont délimités par des caractères accent grave (` `) TOUT CE QUI RENVOIE UNE VALEUR VA ETRE REMPLACE PAR SA VALEUR
-    })
-  
-}
-    let kanapColor= document.querySelector("#colors");       // *** J'ai du creer une variable ( localise l'ID colors) qui me pemrmet de creer une fonction "each" et d'inserer la couleur ensuite    
-    let addToTheCart = document.getElementById("addToCart");
-    let quantity  = document.getElementById("quantity");
+      function addBasket(kanapItem) {                                                   // Fonction dans la fonction qui me permetra de rajouter un element
+        let basket = JSON.parse(localStorage.getItem("localBasket"));                   // Creation d'un variable qui permet de recuperer les infos se trouvant dans le localStorage nommé basket
 
-async function displayInfo () {                                       // Creation d'une fonction qui permet l'affichage dans la page
-   await productData();
-
-    
-    addToTheCart.addEventListener('click', (event) => {   
-        event.preventDefault();
-        if (kanapColor.value == "") {                            // Je m'assure qu'il y est une valeur existante (couleur du canap voir plus haut)
-            alert("Vous avez oubliez de choisir la couleur de votre nouveau canapé");
-            
-       } else if (quantity.value <= 0 || quantity.value > 100   ||  quantity.value == "" ) {                                   // Je m'assure que la quantité de canapé choisi soit differente de "0"
-                alert("Vous avez oubliez de mentionner le nombre de canapé que vous désirez");
-              
-           }  else {    
-            let kpColorV = kanapColor.value < 0 ;
-            let qty = quantity.value;
-
-            const kanapItem = {
-                id: productID,
-                img: product.imageUrl,
-                alt: product.altTxt,
-                description: product.description,
-                name: product.name,
-                quantity: qty,
-                color: kpColorV
-  };
-  let basket = JSON.parse(localStorage.getItem("localStorageBasket"));  // permet d'obtenir la valeur de l'élément de stockage local spécifié ** basket sera le nom du stockage avec pour clé "localstorageBasket"
-
-            addBasket = () => localStorage.setItem("localStorageBasket", JSON.stringify(basket));  // JSON.stringify va permetre de transformer un tableau ou objet complexe en chaine de charactere. *** le "basket" est la valeur
-//                                                                                                    elle permet d'ajouter à l'emplacement de stockage ou mettre a jour sa valeur
-//                                                                                                    le localstorage.setItem me specifie que "localStorageBasket" (sa clé) est le lieu de stockage où je vais prendre un fichier à ....
-            nextLevel= () =>  {                                  // permet si tout est bon d'aller vers la page du panier
-                  if (confirm("Etes vous pret à passer l'étape suivante") == true) {
-                    location.href = "../html/cart.html";        // lien de la page panier
-                } else {
-                    alert("Vous pouvez continuer votre shopping");  // l'informer qu'elle peut continuer ses achats
-                }
+        if (basket == null) {                                                           // Si le localstiorage est vide 
+            basket= [];                                                                 // Alors création d'un tableau 
+            basket.push(kanapItem);                                                     // La méthode push() ajoute de nouveaux éléments à la fin d'un tableau, modifie la longueur du tableau et renvoie la nouvelle longueur.
+          localStorage.setItem("localBasket", JSON.stringify(basket));                  // L'objet localStorage STOCKE les données sans date d'expiration et permet d'enregistrer des paires clé/valeur dans le navigateur.
+          alert("Votre canapé a été ajouté à votre panier");                            // La methode alert()affiche une boîte d'alerte avec un message et un bouton OK, elle est utilisée lorsque vous souhaitez que des infos parviennent à l'utilisateur. Ne pas en Abuser
+        } 
+        else {                                                                          // sinon 
+          let update = false;                                                           // 
+          for (let i = 0; i < basket.length; i++) {                                     // Les boucles for sont pratiques pour exécuter le même code encore et encore, à chaque fois avec une valeur différente.
+            if (kanapItem.id == basket[i].id && kanapItem.color == basket[i].color) {   // Si l'id demandé est egale a celle du panier, si la couleur demandé est égale a celle du panier
+              update = true;                                                            // la mise a jour est donc "possible"
+              let totalBasket = parseInt(basket[i].quantity) + parseInt(kanapItem.quantity); // La méthode parseInt analyse une valeur sous forme de chaîne et renvoie le premier entier. Mon total sera = ma demande sur l'ecran + ce qui est deja dans le panier
+              basket[i].quantity = totalBasket;                                         // le contenu de mon panier me donnera le nouveau total 
+              alert("la quantité choisie a été mise à jour");
             }
-            
-            if (basket) {              // si mon stock nommé basket est vide on lui rajoute un tableau * si il y a deja un element on lui en rajoute un (push)
-
-               // let foundProduct = basket.find((elt) => elt.productID === productID && elt.color === kpColorV); // .find va me permettre de trouver le produit avec l'id je recherche dans le panier
-                // let foundProduct = basket.find(elt => elt.productID === productID && elt.color === kpColorV);  
-                if (basket == null) {                 // si mon panier est vide alors il retournera un tableau vide
-                    return [];
-                } else {
-                    basket.push(kanapItem);     //  si il y a deja un tableau ou un element alors on va rajouter (push)
-                    addBasket();                
-                    nextLevel();                // rappel des fonctions
-                }
-              }  
-    }});
+          } if (update == false) {
+            basket.push(kanapItem);                                                     // alors poussé l'element (kanapItem) dans le localStorage 
+            alert("Votre nouveau canapé a été ajouté à votre panier");
+          }
+          localStorage.setItem("localBasket", JSON.stringify(basket));                  // On redefinit la valeur (basket) du stockage (localstorage) qui a pour clé (localBasket)
+        }
+      }   addBasket(kanapItem);                                                         // appel de la fonction qui permet d'executer ses instructions
+    }
+    else {
+      alert("N'oubliez pas de nous informer sur la couleur et le nombre d'article de votre choix");
+    }
+  })
 }
-
-displayInfo();     // appel de la fonction display sinon elle n'apparaitra pas 
-
-
-
-// Avec jQuery $(selector).action() pour assigner un élément à un évènement. 
-// Les gabarits sont délimités par des caractères accent grave (` `) au lieu des apostrophes doubles ou simples. Les gabarits peuvent contenir des espaces réservés (placeholders). Ces espaces sont indiqués par le signe dollar ($) et des accolades (${expression}). Les expressions dans les espaces réservés et le texte compris dans ces espaces sont passés à une fonction.
-
-/*
-
-if (foundProduct) {
-
-    //let addQuantity = parseInt(kanapItem.quantity) + parseInt(foundProduct.quantity);
-  //  foundProduct.quantity = addQuantity;
-
-    addBasket();
-    nextLevel();
-}
-// else {
-  //  basket.push(kanapItem);
-
-    //addBasket();
-   // nextLevel();
-
-// }
-
-} else {
-basket = [];
-basket.push(kanapItem);
-
-addBasket();
-nextLevel();
-}
-
-
-*/
+displayInfo();                                                                          // appel de la fonction qui permet d'executer ses instructions
